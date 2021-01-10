c34c7f0837ee33f660a91a58d49b9df72b7bbf2d
mgonzalesaguirre <mgonzalesaguirre@hotmail.com>
2021-01-10 00:17:09 -0500

    feat(1b): Crear componentes de página  y delegar la gestión de rutas a sus respectivos módulos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+      },
+      {
+        path: 'course',
+        loadChildren: () => import('./modules/course/course.module').then(m => m.CourseModule)
+      },
+      {
+        path: 'contact',
+        loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
+      },
+      {
+        path: 'admin',
+        loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 
